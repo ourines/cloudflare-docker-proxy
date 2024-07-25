e0a1230bed18f731b50dd1814d675ac1e31eb4b1
ourines <ourines@icloud.com>
2024-07-25 12:50:30 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.ourines.com": dockerHub,
+  "quay.ourines.com": "https://quay.io",
+  "gcr.ourines.com": "https://gcr.io",
+  "k8s-gcr.ourines.com": "https://k8s.gcr.io",
+  "k8s.ourines.com": "https://registry.k8s.io",
+  "ghcr.ourines.com": "https://ghcr.io",
+  "cloudsmith.ourines.com": "https://docker.cloudsmith.io",
+  "ecr.ourines.com": "https://public.ecr.aws",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
